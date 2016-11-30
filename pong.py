550b7ba5cacdd02d2a5c60ae810f81db21405adc
Deric Pang <dericp@cs.washington.edu>
2016-11-30 14:05:37 +0100

    Press space to start game.

--- pong.py
+++ pong.py
@@ -4,7 +4,7 @@ import random
 FPS = 60
 CLOCK = pygame.time.Clock()
 
-WINDOW_WIDTH = 400
+WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 400
 
 PADDLE_WIDTH = 10
@@ -14,7 +14,8 @@ PADDLE_BUFFER = 10
 BALL_WIDTH = 10
 BALL_HEIGHT = 10
 
-PADDLE_SPEED = 3
+AI_PADDLE_SPEED = 2
+PLAYER_PADDLE_SPEED = 5
 BALL_X_SPEED = 6
 BALL_Y_SPEED = 4
 
@@ -39,43 +40,39 @@ def draw_paddle_2(y_pos):
     pygame.draw.rect(screen, WHITE, paddle2)
 
 
-def update_ball_pos(ball_x_pos, ball_y_pos, ball_x_dir, ball_y_dir, paddle_left_y_pos, paddle_right_y_pos):
-    ball_x_pos = ball_x_pos + ball_x_dir * BALL_X_SPEED
-    ball_y_pos = ball_y_pos + ball_y_dir * BALL_Y_SPEED
+def update_ball_pos(game):
+    game.ball_x_pos = game.ball_x_pos + game.ball_x_dir * BALL_X_SPEED
+    game.ball_y_pos = game.ball_y_pos + game.ball_y_dir * BALL_Y_SPEED
 
     # if ball hits left paddle
-    if (ball_x_pos <= PADDLE_BUFFER + PADDLE_WIDTH and ball_y_pos + BALL_HEIGHT >= paddle_left_y_pos and ball_y_pos -
-            BALL_HEIGHT <= paddle_left_y_pos + PADDLE_HEIGHT):
-        ball_x_dir = 1
+    if (game.ball_x_pos <= PADDLE_BUFFER + PADDLE_WIDTH and game.ball_y_pos + BALL_HEIGHT >= game.paddle_left_y_pos and game.ball_y_pos -
+            BALL_HEIGHT <= game.paddle_left_y_pos + PADDLE_HEIGHT):
+        game.ball_x_dir = 1
     # if we got scored on
-    elif ball_x_pos <= 0:
+    elif game.ball_x_pos <= 0:
         # potentially reset game here
-        ball_x_dir = 1
-        return [ball_x_pos, ball_y_pos, ball_x_dir, ball_y_dir, paddle_left_y_pos, paddle_right_y_pos]
+        scored_on(game)
 
     # if ball hits right paddle
-    if (ball_x_pos >= WINDOW_WIDTH - PADDLE_WIDTH - PADDLE_BUFFER - BALL_WIDTH and ball_y_pos + BALL_HEIGHT >=
-        paddle_right_y_pos and ball_y_pos - BALL_HEIGHT <= paddle_right_y_pos + PADDLE_HEIGHT):
-        ball_x_dir = -1
-    elif ball_x_pos >= WINDOW_WIDTH - BALL_WIDTH:
-        ball_x_dir = -1
-        return [ball_x_pos, ball_y_pos, ball_x_dir, ball_y_dir, paddle_left_y_pos, paddle_right_y_pos]
+    if (game.ball_x_pos >= WINDOW_WIDTH - PADDLE_WIDTH - PADDLE_BUFFER - BALL_WIDTH and game.ball_y_pos + BALL_HEIGHT >=
+        game.paddle_right_y_pos and game.ball_y_pos - BALL_HEIGHT <= game.paddle_right_y_pos + PADDLE_HEIGHT):
+        game.ball_x_dir = -1
+    elif game.ball_x_pos >= WINDOW_WIDTH - BALL_WIDTH:
+        scored_on(game)
 
-    if ball_y_pos <= 0:
-        ball_y_pos = 0
-        ball_y_dir = 1
-    elif ball_y_pos >= WINDOW_HEIGHT - BALL_HEIGHT:
-        ball_y_pos = WINDOW_HEIGHT - BALL_HEIGHT
-        ball_y_dir = -1
-
-    return (ball_x_pos, ball_y_pos, ball_x_dir, ball_y_dir, paddle_left_y_pos, paddle_right_y_pos)
+    if game.ball_y_pos <= 0:
+        game.ball_y_pos = 0
+        game.ball_y_dir = 1
+    elif game.ball_y_pos >= WINDOW_HEIGHT - BALL_HEIGHT:
+        game.ball_y_pos = WINDOW_HEIGHT - BALL_HEIGHT
+        game.ball_y_dir = -1
 
 
 def update_ai_paddle_pos(paddle_y_pos, ball_y_pos):
-    if ball_y_pos > paddle_y_pos + PADDLE_HEIGHT / 2.0:
-        paddle_y_pos = paddle_y_pos + PADDLE_SPEED
-    elif ball_y_pos < paddle_y_pos:
-        paddle_y_pos = paddle_y_pos - PADDLE_SPEED
+    if ball_y_pos > paddle_y_pos + PADDLE_HEIGHT / 2.0 + 10:
+        paddle_y_pos = paddle_y_pos + AI_PADDLE_SPEED
+    elif ball_y_pos < paddle_y_pos + PADDLE_HEIGHT / 2.0 - 10:
+        paddle_y_pos = paddle_y_pos - AI_PADDLE_SPEED
 
     if paddle_y_pos < 0:
         paddle_y_pos = 0
@@ -87,10 +84,10 @@ def update_ai_paddle_pos(paddle_y_pos, ball_y_pos):
 
 def update_player_paddle_pos(paddle_y_pos, keys):
     if keys[pygame.K_DOWN]:
-        paddle_y_pos = paddle_y_pos + PADDLE_SPEED
+        paddle_y_pos = paddle_y_pos + PLAYER_PADDLE_SPEED
 
     if keys[pygame.K_UP]:
-        paddle_y_pos = paddle_y_pos - PADDLE_SPEED
+        paddle_y_pos = paddle_y_pos - PLAYER_PADDLE_SPEED
 
     if paddle_y_pos < 0:
         paddle_y_pos = 0
@@ -100,8 +97,39 @@ def update_player_paddle_pos(paddle_y_pos, keys):
     return paddle_y_pos
 
 
+def scored_on(game):
+    game.playing = False
+    reset_game(game)
+
+
+def reset_game(game):
+    #game.paddle_left_y_pos = WINDOW_HEIGHT / 2 - PADDLE_HEIGHT / 2
+    #game.paddle_right_y_pos = WINDOW_HEIGHT / 2 - PADDLE_HEIGHT / 2
+
+    game.ball_x_dir = 1
+    game.ball_y_dir = 1
+    game.ball_x_pos = int(WINDOW_WIDTH / 2.0 - BALL_WIDTH / 2.0)
+    game.ball_y_pos = int(WINDOW_HEIGHT / 2.0 - BALL_HEIGHT / 2.0)
+
+    num = random.randint(0, 9)
+    if(0 < num < 3):
+        game.ball_x_dir = 1
+        game.ball_y_dir = 1
+    if (3 <= num < 5):
+        game.ball_x_dir = -1
+        game.ball_y_dir = 1
+    if (5 <= num < 8):
+        game.ball_x_dir = 1
+        game.ball_y_dir = -1
+    if (8 <= num < 10):
+        game.ball_x_dir = -1
+        game.ball_y_dir = -1
+
+
 class Pong:
     def __init__(self):
+        self.playing = False
+
         self.paddle_left_y_pos = WINDOW_HEIGHT / 2 - PADDLE_HEIGHT / 2
         self.paddle_right_y_pos = WINDOW_HEIGHT / 2 - PADDLE_HEIGHT / 2
 
@@ -130,19 +158,12 @@ class Pong:
         screen.fill(BLACK)
         keys = pygame.key.get_pressed()
 
-        # update ball
-        (self.ball_x_pos,
-         self.ball_y_pos,
-         self.ball_x_dir,
-         self.ball_y_dir,
-         self.paddle_left_y_pos,
-         self.paddle_right_y_pos) = \
-            update_ball_pos(self.ball_x_pos,
-                            self.ball_y_pos,
-                            self.ball_x_dir,
-                            self.ball_y_dir,
-                            self.paddle_left_y_pos,
-                             self.paddle_right_y_pos)
+        if not self.playing:
+            if keys[pygame.K_SPACE]:
+                self.playing = True
+            reset_game(self)
+        else:
+            update_ball_pos(self)
 
         draw_ball(self.ball_x_pos, self.ball_y_pos)
 
